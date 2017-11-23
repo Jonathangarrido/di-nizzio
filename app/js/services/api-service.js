df53135fd340a41bf57e0ce5715b8b7936c60050
Jonathan Garrido <jonathan.garrido1988@gmail.com>
2017-11-23 08:10:07 -0300

    Update api-service.js

--- app/js/services/api-service.js
+++ app/js/services/api-service.js
@@ -64,7 +64,7 @@
       var productos = [
         {
           id: 'spaghetti-putanesca',
-          imagen: 'spaguetti-putanesca.jpg',
+          imagen: 'unnamed.jpg',
           titulo: 'Spaghetti Putanesca',
           ingredientes: ' 680 grs. de tomates frescos y maduros o 650 grs. de tomates enlatados-- Sal gruesa para cocinar la pasta y sazonar la salsa-- 2 dientes de ajos medianos pelados-- 1/2 taza de aceite de oliva-- 220 grs. de aceitunas negras amargas de Azapa en salmuera-- 3 anchoas en sal o 6 filetes de anchoas en aceite-- 4 cucharadas de alcaparras en vinagre-- 2 cucharadas de pasta de tomates-- Pimienta fresca-- 1/2 cucharadita de té de hojuelas de pimiento rojo-- 450 grs. de spaghetti, preferentemente importado de Italia',
           preparacion: 'Si se van a usar tomates frescos, blanquearlos en agua hirviendo con sal. Sacarles la cáscara y cortarlos en pedazos. Luego, pasarlos por un prensa puré y ponerlos en un bowl de vidrio. Reservar.--Cortar el ajo en pedazos grandes. Calentar el aceite en un sartén a temperatura media, echar los ajos y saltearlos entre 2 a 3 minutos o hasta que tomen un color dorado claro.--Cortar los filetes de anchoas en pedazos de 2,5 cms. cada uno. Posteriormente, agregar los tomates a la sartén, luego las aceitunas, anchoas, alcaparras y la pasta de tomates. Mezclar bien y sazonar con sal, pimienta y hojuelas de pimiento rojo. Cocinar por 15 minutos a temperatura media revolviendo de vez en cuando con una cuchara de palo.--Cocinar la pasta en agua hirviendo al dente, por 9 a 12 minutos. Colarla, ponerla en una fuente grande, echar la salsa encima y mezclar bien. Por último, cambiar la pasta a otra fuente previamente entibiada y servir de inmediato.'
