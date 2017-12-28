700bc0a017ab712be1e449bda7f739ee61ab613c
jonathan garrido <jonathan.garrido1988@gmail.com>
2017-12-28 00:16:20 -0300

    bug slider video

--- app/js/components/home/mi-home.js
+++ app/js/components/home/mi-home.js
@@ -20,6 +20,33 @@ function miHomeCtrl(Consultas,Analytics,Modal) {
   //
 
   function slider(){
+
+    $('.bxslider-videos').bxSlider({
+      mode: 'horizontal', //'horizontal', 'vertical', 'fade'
+      speed: 500, // speed animation
+      pager: false, // icon pager
+      auto: false,
+      pause: 4000, // speed transition
+      infiniteLoop: false,
+      hideControlOnEnd: true,
+      controls: true, // icon arrow
+      nextText:'',
+      prevText:'',
+      onSlideBefore: function($slideElement, oldIndex, newIndex){
+        videos(oldIndex);
+      }
+    });
+
+    $('.bxslider-videos li').each(function(){
+
+      var iframe = $(this).html();
+      console.log(iframe)
+      var newifram = iframe.replace('iframeoculto','');
+      $(this).find('.videosIframe').html(newifram)
+      $(this).find('.videosIframe .videosIframe').fadeOut()
+      $(this).find('.iframeoculto').fadeOut()
+    })
+
     var ancho = jQuery(window).width(),
         bxMinMaxSlides = 1;
   
@@ -33,7 +60,7 @@ function miHomeCtrl(Consultas,Analytics,Modal) {
       bxMinMaxSlides = 4;
     }
   
-    $('.bxslider').bxSlider({
+    $('.bxslider2').bxSlider({
       mode: 'horizontal', //'horizontal', 'vertical', 'fade'
       speed: 500, // speed animation
       pager: false, // icon pager
@@ -50,19 +77,21 @@ function miHomeCtrl(Consultas,Analytics,Modal) {
       slideWidth: 270
     });
 
-    $('.bxslider-videos').bxSlider({
-      mode: 'horizontal', //'horizontal', 'vertical', 'fade'
-      speed: 500, // speed animation
-      pager: false, // icon pager
-      auto: false,
-      pause: 4000, // speed transition
-      infiniteLoop: false, 
-      hideControlOnEnd: true,
-      controls: true, // icon arrow
-      nextText:'',
-      prevText:'',
-    });
+    
   }
+
+  function videos(posicion){
+    console.log('function videos')
+    if($('.actividades').length === 1){
+      var atras = posicion + 1;
+      var conteBefore = $('.bxslider-videos li:nth-child('+atras+')');
+      var ifra = conteBefore.find('.videosIframe').html();
+      conteBefore.find('iframe').remove();
+      conteBefore.find('.videosIframe').html(ifra);
+    }
+    
+  }
+  
 
   //obtengo slider
   function getSlider() {
